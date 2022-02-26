f3ce056a27533356fbce3b79ba5a871b2edbfb51
Ben Steinberg <benjamin.steinberg@mail.huji.ac.il>
2022-02-26 16:47:03 +0200

    added ovation rrbs script

---
+++ scripts_2022/analyze_ovation_rrbs_pbs_job.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set_software_paths()
+{
+	#cutadapt version 3.7
+	#cutadapt is a python package installed in PYTHON_ENV.	PYTHON_ENV=/home/s.benjamin/bioinformatics_software/ovation-rrbs-methyl-seq__python3-env
+	
+	#trim_galore version 0.6.8 (15 01 2022)
+	TRIM_GALORE=/home/s.benjamin/bioinformatics_software/TrimGalore/trim_galore
+	
+	#Bowtie 2 version 2.2.3
+	BOWTIE2=/usr/local/bin/bowtie2
+	
+	#samtools version 0.1.19-44428cd
+	SAMTOOLS=/usr/local/samtools/bin/samtools
+	
+	#Bismark Version: v0.23.1
+	BISMARK=/home/s.benjamin/bioinformatics_software/Bismark-0.23.1/bismark
+	
+	#java version "1.8.0_202"
+	JAVA=/usr/local/matlab2021b/sys/java/jre/glnxa64/jre/bin/java
+	
+	#FastQC v0.11.9
+	FASTQC=/home/s.benjamin/bioinformatics_software/FastQC/fastqc
+	
+	
+	#add paths to executables
+	ADD_TO_PATH=""
+	for executable in $TRIM_GALORE $BOWTIE2 $SAMTOOLS $BISMARK $JAVA  $FASTQC
+	do
+		
+		ADD_TO_PATH+=$(echo $executable | awk -F / 'NF{NF--};{OFS = FS; print $0}'):
+	done
+	echo $ADD_TO_PATH
+	export PATH="$ADD_TO_PATH:$PATH"
+}
+
+trim_illumina_adapter_single_end()
+{
+	#first positional argument is fastq file to trim
+	${TRIM_GALORE} --adapter AGATCGGAAGAGC $1  
+}
+
+main()
+{
+	set_software_paths
+	
+	#activate python virtual enviorment
+	source ${PYTHON_ENV}/bin/activate 
+		trim_illumina_adapter_single_end $input_fastq_file
+		
+	
+
+
+	deactivate #deactivate python virtual enviorment
+}
