4455f50e30ac9c20c2abb2e7593434e36ec11936
Ben Steinberg <benjamin.steinberg@mail.huji.ac.il>
2022-03-03 13:48:38 +0200

    working on ovation rrbs script

--- scripts_2022/send_ovation_rrbs_jobs.sh
+++ scripts_2022/send_ovation_rrbs_jobs.sh
@@ -4,8 +4,10 @@ main()
 {
 	arg_parse "$@"
 	
-	if [[ ! -d $OUTPUT_DIR ]] then
-		do	mkdir $OUTPUT_DIR
+	script=/home/s.benjamin/bioinformatics_software/repo_for_reizel_lab/scripts_2022/analyze_ovation_rrbs_pbs_job.sh
+	
+	if [[ ! -d $OUTPUT_DIR ]]; then
+		mkdir $OUTPUT_DIR
 	fi
 	
 	cd $OUTPUT_DIR
@@ -16,7 +18,21 @@ main()
 		mkdir $dir_name
 		cd $dir_name
 		#TODO: send_job_from_here
-		echo analyze_ovation_script_single_sample $READ_TYPE -input_fastq_file $sample | tee $dir_name.log
+		cat << EOF
+#!/bin/sh
+#PBS  -N  ovation_rrbs_${dir_name}
+#PBS  -q  queue_name
+#PBS  -m  abe
+#PBS  -M  s.benjamin@technion.ac.il
+#PBS  -l select=1:ncpus=20
+#PBS  -l select=mem=64 GB
+#PBS  -l walltime=24:00:00
+PBS_O_WORKDIR=$dir_name
+cd $PBS_O_WORKDIR
+
+./program.exe < input.file > output.file 2>&1
+$script $READ_TYPE -input_fastq_file $sample > output.file 2>&1 $dir_name.log
+EOF
 		cd ..
 	done
 }
