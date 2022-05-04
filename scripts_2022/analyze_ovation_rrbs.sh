799015b0c47d62012c95458df2e6db03ee859167
Ben <ben.g.steinberg@gmail.com>
2022-05-04 15:47:12 +0300

    adding paired end support

--- scripts_2022/analyze_ovation_rrbs.sh
+++ scripts_2022/analyze_ovation_rrbs.sh
@@ -92,8 +92,8 @@ align_to_genome()
     TRIM_DIVERSITY_OUTPUT=$(echo $TRIM_GALORE_OUTPUT | sed 's/\.gz/_trimmed.fq.gz/')
     COMMAND=$(echo $BISMARK --multicore $N_CORES --bowtie2 $BISMARK_GENOME_LOCATION $TRIM_DIVERSITY_OUTPUT)
 	else
-    TRIM_DIVERSITY_OUTPUT_1=$(echo $TRIM_GALORE_TRIM_GALORE_OUTPUT_1 | sed 's/\.gz/_trimmed.fq.gz/')
-    TRIM_DIVERSITY_OUTPUT_2=$(echo $TRIM_GALORE_TRIM_GALORE_OUTPUT_2 | sed 's/\.gz/_trimmed.fq.gz/')
+    TRIM_DIVERSITY_OUTPUT_1=$(echo $TRIM_GALORE_OUTPUT_1 | sed 's/\.gz/_trimmed.fq.gz/')
+    TRIM_DIVERSITY_OUTPUT_2=$(echo $TRIM_GALORE_OUTPUT_2 | sed 's/\.gz/_trimmed.fq.gz/')
     COMMAND=$(echo $BISMARK --multicore $N_CORES --bowtie2 $BISMARK_GENOME_LOCATION -1 $TRIM_DIVERSITY_OUTPUT_TRIM_GALORE_OUTPUT_1 -2 TRIM_DIVERSITY_OUTPUT_2)
 	fi
 
