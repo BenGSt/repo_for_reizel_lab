f2f6aebe354bed7948c383d7974beeeeef7f2b24
Ben Steinberg <benjamin.steinberg@mail.huji.ac.il>
2022-05-29 11:06:13 +0300

    -p 1

--- scripts_2022/homer.sh
+++ scripts_2022/homer.sh
@@ -2,7 +2,7 @@
 #USAGE: $0 <regions_file_mm10.bed> <bg_file.bed>
 #results dir in pwd
 
-reference_genome=/utemp/s.benjamin/genomic_reference_data/from_huji/mm10/Sequence/WholeGenomeFasta/genome.fa
+reference_genome=/storage/bfe_reizel/bengst/genomic_reference_data/from_huji/mm10/Sequence/WholeGenomeFasta/genome.fa
 
 #make bg (superset of test regions random tiles):
 bedtools getfasta -fi $reference_genome -bed $2  -fo $(echo $2 | sed 's/.bed/.fa/')
@@ -14,4 +14,4 @@ bedtools getfasta -fi $reference_genome -bed $2  -fo $(echo $2 | sed 's/.bed/.fa
 bedtools getfasta -fi $reference_genome -bed $1 -fo `echo $1 | sed s/".bed"/".fa"/`
 
 #find motifs with  background
-findMotifs.pl `echo $1 | sed s/".bed"/".fa"/` fasta `echo $1 | sed s/".bed"/"_HOMER_RTESULTS"/` -p 20 -fastaBg $(echo $2 | sed 's/.bed/.fa/') 
+findMotifs.pl `echo $1 | sed s/".bed"/".fa"/` fasta `echo $1 | sed s/".bed"/"_HOMER_RTESULTS"/` -p 1 -fastaBg $(echo $2 | sed 's/.bed/.fa/') 
