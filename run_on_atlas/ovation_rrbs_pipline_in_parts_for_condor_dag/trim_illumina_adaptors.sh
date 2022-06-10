6ead022164e72b3ff0913e1b36bf2c7afee3d19f
Ben <ben.g.steinberg@gmail.com>
2022-06-10 20:01:54 +0300

    adapting rrbs pipline to condor dag

--- run_on_atlas/ovation_rrbs_pipline_in_parts_for_condor_dag/trim_illumina_adaptors.sh
+++ run_on_atlas/ovation_rrbs_pipline_in_parts_for_condor_dag/trim_illumina_adaptors.sh
@@ -26,7 +26,6 @@ main()
 	echo
 	echo
 
-  source set_software_paths.sh
 	arg_parse "$@"
 	set_software_paths
 
@@ -79,6 +78,35 @@ trim_illumina_adapter_single_end()
 	${TRIM_GALORE} --adapter AGATCGGAAGAGC $1  --cores $n_cores --fastqc
 	echo \########################################################
 }
+
+
+set_software_paths()
+{
+  #	echo setting software paths
+	# on Atlas I'm using conda for env managment, so the software should be in path already.
+	# leaving this function if I want to change something without the env.
+	#TODO: consider using calls to program name from the rest of the script and delete this function.
+
+	PYTHON3=python
+
+	TRIM_GALORE=trim_galore
+
+	BOWTIE2=bowtie2
+
+	SAMTOOLS=samtools
+
+	BISMARK=bismark
+
+	FASTQC=fastqc
+
+	DIVERSITY_TRIM_SCRIPT=/Local/bfe_reizel/anaconda3/envs/ovation_rrbs_pipeline_2022/bin/trimRRBSdiversityAdaptCustomers.py
+
+	BEDTOOLS=bedtools
+
+	#tecan nudup tool for pcr duplicates
+	NUDUP=/Local/bfe_reizel/anaconda3/envs/ovation_rrbs_pipeline_2022/bin/nudup.py
+}
+
 
 
 arg_parse()
