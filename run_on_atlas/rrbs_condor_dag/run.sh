d3bb610c124004e35d9a5bc927bf4fc99c0e66a1
Ben <ben.g.steinberg@gmail.com>
2022-06-15 17:03:55 +0300

    adapting rrbs pipline to condor dag

--- run_on_atlas/rrbs_condor_dag/run.sh
+++ run_on_atlas/rrbs_condor_dag/run.sh
@@ -47,7 +47,7 @@ executables=(
             combine_methylation_coverage_to_tiles.sh \
 )
 cores=(8 1 10 10 1)
-rams=(300MB 10MB 16GB 6GB 500MB)
+rams=(300MB 10MB 16GB 6GB 2GB)
 
 for i in "${!submission_file_names[@]}"; do
 
