7e318f104b44ba27ef680b4cb700ce485f0b1184
Ben <ben.g.steinberg@gmail.com>
2022-07-14 16:39:20 +0300

    fix file names and plot titles from 25p to meth_difference"p"

--- run_on_atlas/dmrs_condor_dag/homer_job.sh
+++ run_on_atlas/dmrs_condor_dag/homer_job.sh
@@ -5,7 +5,7 @@ source /Local/bfe_reizel/anaconda3/bin/activate dmrs_pipeline_2022
 function main()
 {
     cd $1
-    for dmrs in dmrs_25p_hyper.bed dmrs_25p_hypo.bed; do
+    for dmrs in $(ls| grep -P 'dmrs_[0-9]*p_hyper.bed|dmrs_[0-9]*p_hypo.bed'); do
         /srv01/technion/bengst/scripts/repo_for_reizel_lab/scripts_2022/homer.sh $dmrs $1_dmrs_plus_random_50000_100bp_tiles.bed
     done
 }
--- scripts_2022/homer.sh
+++ scripts_2022/homer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #USAGE: $0 <regions_file_mm10.bed> <bg_file.bed>
 #results dir in pwd
+N_CORES=10
+
 
 reference_genome=/storage/bfe_reizel/bengst/genomic_reference_data/from_huji/mm10/Sequence/WholeGenomeFasta/genome.fa
 
@@ -14,4 +16,4 @@ bedtools getfasta -fi $reference_genome -bed $2  -fo $(echo $2 | sed 's/.bed/.fa
 bedtools getfasta -fi $reference_genome -bed $1 -fo `echo $1 | sed s/".bed"/".fa"/`
 
 #find motifs with  background
-findMotifs.pl `echo $1 | sed s/".bed"/".fa"/` fasta `echo $1 | sed s/".bed"/"_HOMER_RESULTS"/` -p 1 -fastaBg $(echo $2 | sed 's/.bed/.fa/') 
+findMotifs.pl `echo $1 | sed s/".bed"/".fa"/` fasta `echo $1 | sed s/".bed"/"_HOMER_RESULTS"/` -p $N_CORES -fastaBg $(echo $2 | sed 's/.bed/.fa/')
