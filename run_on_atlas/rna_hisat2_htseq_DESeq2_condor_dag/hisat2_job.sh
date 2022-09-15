0d0c0fe6d452731e287121f9bfc4cab63acd07b5
Ben <s.benjamin@technion.ac.il>
2022-09-15 15:30:11 +0300

    add rna-seq condor dag

---
+++ run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/hisat2_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+r1=$1
+r2=$2
+output_file=$3
+summary_file=$4 # {}_hisat2.summary.txt
+hisat2_idx=/storage/bfe_reizel/bengst/genomic_reference_data/hg38/analysisSet/chromosomes_hisat2_index/hisat2-index
+
+source /Local/bfe_reizel/anaconda3/bin/activate rna-seq_hisat2_htseq_deseq2_2022
+mkdir -p $(dirname $summary_file)
+hisat2 -p 10 -x $hisat2_idx -1 $r1 -2 $r2   --summary-file $summary_file | samtools sort -n --output-fmt BAM > $output_file
+#TODO: single end read option
---
+++ run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/run.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+main()
+{
+  if [[ $# -lt 2 ]]; then
+    echo USAGE: $0  \<\raw_data_dir\>
+    echo raw_data_dir should contain a dir for each sample containing it\'s fastq files.
+    echo Run from the directory you wish the output to be written to.
+    exit 1
+  fi
+  write_condor_submition_files $1
+  write_condor_dag
+  mkdir logs
+
+  echo Submit the jobs by running: condor_submit_dag rrbs_jobs.dag
+  echo Good Luck!
+  #TODO: deseq2
+  #TODO: single end read option
+}
+
+write_condor_submition_files()
+{
+  raw_dir=$1
+  cat << EOF > hisat2_jobs.sub
+Initialdir = $(pwd)
+executable = hisat2_job.sh
+Arguments = \$(args)
+request_cpus = 10
+RequestMemory = 8GB
+universe = vanilla
+log = logs/\$(name)_hisat2.log
+output = logs/\$(name)_hisat2.out
+error = logs/\$(name)_hisat2.out
+queue name, args from (
+$( for samp_dir in $(find $raw_dir/* -type d); do echo $samp_dir | awk -F / '{printf $NF", "}'; find $samp_dir | grep -E 'R1|R2'| sort | awk '{printf $0" "}' ; echo $samp_dir | awk -F / '{print "./"$NF"/"$NF".hisat2_output.bam ./"$NF"/"$NF".hisat2.summary.txt "}'  ; done)
+)
+EOF
+
+  cat << EOF > htseq_jobs.sub
+Initialdir = $(pwd)
+executable = htseq_job.sh
+Arguments = \$(args)
+request_cpus = 1
+RequestMemory = 4GB
+universe = vanilla
+log = logs/\$(name)_htseq-count.log
+output = logs/\$(name)_htseq-count.out
+error = logs/\$(name)_htseq-count.out
+queue name, args from (
+$( for samp_dir in $(find $raw_dir/* -type d); do echo $samp_dir | awk -F / '{printf $NF", "}'; echo $samp_dir | awk -F / '{print "./"$NF"/"$NF".hisat2_output.bam ./"$NF"/"$NF".htseq-count_outout.txt"}'  ; done)
+)
+EOF
+}
+
+write_condor_dag()
+{
+    cat << EOF > rrbs_jobs.dag
+JOB align_hisat2 hisat2_jobs.sub
+JOB count_reads_htseq htseq_jobs.sub
+
+PARENT align_hisat2  CHILD count_reads_htseq
+EOF
+}
