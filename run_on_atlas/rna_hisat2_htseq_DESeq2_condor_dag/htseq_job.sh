da33201708724582987a16746c5a59fafcedae0f
Ben <s.benjamin@technion.ac.il>
2022-09-19 10:23:14 +0300

    count against hg38.ensGene.gtf

--- run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/htseq_job.sh
+++ run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/htseq_job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 input_file=$1
 output_file=$2
-GTF=/storage/bfe_reizel/bengst/genomic_reference_data/hg38/hg38.p13/genes/hg38.knownGene.gtf
+#GTF=/storage/bfe_reizel/bengst/genomic_reference_data/hg38/hg38.p13/genes/hg38.knownGene.gtf
+GTF=/storage/bfe_reizel/bengst/genomic_reference_data/hg38/hg38.p13/genes/hg38.ensGene.gtf
 
 source /Local/bfe_reizel/anaconda3/bin/activate rna-seq_hisat2_htseq_deseq2_2022
 htseq-count --stranded no $input_file $GTF > $output_file
