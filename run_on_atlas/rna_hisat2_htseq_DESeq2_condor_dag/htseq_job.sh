d2ef88f248da8cc9ffd0a81b43ee952f80659480
Ben <s.benjamin@technion.ac.il>
2022-09-21 09:55:55 +0300

    mm10 GTF

--- run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/htseq_job.sh
+++ run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/htseq_job.sh
@@ -2,7 +2,7 @@
 input_file=$1
 output_file=$2
 # GTF=/storage/bfe_reizel/bengst/genomic_reference_data/hg38/hg38.p13/genes/hg38.ensGene.gtf
-GTF=/bengst/genomic_reference_data/from_huji/mm10/Annotation/Archives/archive-2014-05-23-16-05-10/Genes/genes.gtf
+GTF=/storage/bfe_reizel/bengst/genomic_reference_data/from_huji/mm10/Annotation/Archives/archive-2014-05-23-16-05-10/Genes/genes.gtf
 
 source /Local/bfe_reizel/anaconda3/bin/activate rna-seq_hisat2_htseq_deseq2_2022
 htseq-count --stranded no $input_file $GTF > $output_file
