90a584da71b8771772d106a41d7791543616e5a6
Ben <s.benjamin@technion.ac.il>
2022-09-30 11:36:15 +0300

    fix dmr dag

--- scripts_2022/homer.sh
+++ scripts_2022/homer.sh
@@ -2,15 +2,11 @@
 #USAGE: $0 <regions_file_mm10.bed> <bg_file.bed>
 #results dir in pwd
 N_CORES=10
-
-
 reference_genome=/storage/bfe_reizel/bengst/genomic_reference_data/from_huji/mm10/Sequence/WholeGenomeFasta/genome.fa
 
 #make bg (superset of test regions random tiles):
 bedtools getfasta -fi $reference_genome -bed $2  -fo $(echo $2 | sed 's/.bed/.fa/')
-	
 	#e.g.: bedtools getfasta -fi $reference_genome -bed ./dmrs_plus_random_50000_100bp_tiles.bed  -fo dmrs_plus_random_50000_100bp_tiles.fa^
- 
 
 #Extract Sequence from bed file:
 bedtools getfasta -fi $reference_genome -bed $1 -fo `echo $1 | sed s/".bed"/".fa"/`
