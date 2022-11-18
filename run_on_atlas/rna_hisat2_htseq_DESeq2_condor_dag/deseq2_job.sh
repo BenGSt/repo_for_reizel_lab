b8ed4e2b9ad6342b92c6f4daf285738240e1cfa2
Ben <s.benjamin@technion.ac.il>
2022-11-18 16:25:02 +0200

    work on rnaseq dag - deseq

--- run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/deseq2_job.sh
+++ run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/deseq2_job.sh
@@ -3,5 +3,5 @@
 # copy all htseq-count ouput files to one dir
 cp `find -name *htseq-count_output.txt` ./htseq_output
 
-source /Local/bfe_reizel/anaconda3/envs/rna_hisat2_htseq_deseq2_2022_v2
+source /Local/bfe_reizel/anaconda3/bin/activate rna_hisat2_htseq_deseq2_2022_v2
 Rscript /srv01/technion/bengst/scripts/repo_for_reizel_lab/scripts_2022/run_deseq2.R "$@"
