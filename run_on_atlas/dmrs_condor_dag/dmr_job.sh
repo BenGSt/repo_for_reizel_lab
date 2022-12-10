f4073da9dc56f42e1c4fffda1c87527ee34c75e5
Ben <s.benjamin@technion.ac.il>
2022-12-10 18:16:18 +0200

    use $REPO_FOR_REIZEL_LAB which is set in run.sh for each pipline. and added to job env in sub file when appropriate.

--- run_on_atlas/dmrs_condor_dag/dmr_job.sh
+++ run_on_atlas/dmrs_condor_dag/dmr_job.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 source /Local/bfe_reizel/anaconda3/bin/activate dmrs_pipeline_2022
-echo Running: Rscript  ~/scripts/repo_for_reizel_lab/scripts_2022/find_dmrs_methylkit.R "$@"
-Rscript  ~/scripts/repo_for_reizel_lab/scripts_2022/find_dmrs_methylkit.R "$@"
+echo Running: Rscript  $REPO_FOR_REIZEL_LAB/scripts_2022/find_dmrs_methylkit.R "$@"
+Rscript  $REPO_FOR_REIZEL_LAB/scripts_2022/find_dmrs_methylkit.R "$@"
