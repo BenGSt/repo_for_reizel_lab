77ce84f700896a41ce85212188e55bafe0149633
Ben <s.benjamin@technion.ac.il>
2022-12-30 13:38:56 +0200

    build bismark WGBS pipeline

--- run_on_atlas/bismark_wgbs/bismark_align.sh
+++ run_on_atlas/bismark_wgbs/bismark_align.sh
@@ -22,6 +22,7 @@ EOF
 
 main()
 {
+  source /Local/bfe_reizel/anaconda3/bin/activate ovation_rrbs_pipeline_2022
   arg_parse "$@"
 	cd "$output_dir" || exit 1
 	script_name=$(echo $0 | awk -F / '{print $NF}')
