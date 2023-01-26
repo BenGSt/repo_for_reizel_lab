04c127eb1da073bf21675e90019b7e65dedc2be3
Ben <s.benjamin@technion.ac.il>
2023-01-26 11:12:13 +0200

    arg_parse

--- run_on_atlas/bismark_wgbs/make_tiles.sh
+++ run_on_atlas/bismark_wgbs/make_tiles.sh
@@ -16,6 +16,7 @@ EOF
 
 main()
 {
+  arg_parse
   source /Local/bfe_reizel/anaconda3/bin/activate wgbs_bismark_pipeline_2023
 	cd $output_dir || printf "error: output_dir=%s" $output_dir; exit 1
 	script_name=$(echo $0 | awk -F / '{print $NF}')
