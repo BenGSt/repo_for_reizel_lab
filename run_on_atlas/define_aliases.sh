aecb0746c236db49f4676cd93fba9749a38bed4b
Ben <s.benjamin@technion.ac.il>
2023-03-25 14:32:45 +0300

    update

--- run_on_atlas/define_aliases.sh
+++ run_on_atlas/define_aliases.sh
@@ -14,7 +14,7 @@ echo "" >> $USER_HOME/.bashrc
 echo "# Custom Aliases" >> $USER_HOME/.bashrc
 echo "alias wgbs='/storage/bfe_reizel/bengst/repo_for_reizel_lab/run_on_atlas/bismark_wgbs/run_1_dag_per_sample.sh'" >> $USER_HOME/.bashrc
 echo "alias rrbs='/storage/bfe_reizel/bengst/repo_for_reizel_lab/run_on_atlas/rrbs_condor_dag/run.sh'" >> $USER_HOME/.bashrc
-echo "alias alias dmrs='/storage/bfe_reizel/bengst/repo_for_reizel_lab/run_on_atlas/dmrs_condor_dag/run.sh'" >> $USER_HOME/.bashrc
+echo "alias dmrs='/storage/bfe_reizel/bengst/repo_for_reizel_lab/run_on_atlas/dmrs_condor_dag/run.sh'" >> $USER_HOME/.bashrc
 echo "alias rna_seq='/storage/bfe_reizel/bengst/repo_for_reizel_lab/run_on_atlas/rna_hisat2_htseq_DESeq2_condor_dag/run.sh'" >> $USER_HOME/.bashrc
 
 # Reload the .bashrc file
