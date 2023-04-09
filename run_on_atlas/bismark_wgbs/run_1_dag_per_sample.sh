629e5c999a9e284cd461c881ab26598572b5d94d
Ben <s.benjamin@technion.ac.il>
2023-04-09 11:43:03 +0300

    don't rewrite cmd.txt if no args

--- run_on_atlas/bismark_wgbs/run_1_dag_per_sample.sh
+++ run_on_atlas/bismark_wgbs/run_1_dag_per_sample.sh
@@ -109,9 +109,11 @@ EOF
 }
 
 main() {
-  echo \# the command used to prepare the jobs. Note that parentheses are lost > cmd.txt
-  echo \# and need to be added to rerun: -extra-trim-galore-options \"multiple quoted options\" >> cmd.txt
-  echo "$0" "$@" >> cmd.txt #TODO: preserve quotes that may be in args
+  if [[ $# -neq 0 ]]; then
+    echo \# the command used to prepare the jobs. Note that parentheses are lost > cmd.txt
+    echo \# and need to be added to rerun: -extra-trim-galore-options \"multiple quoted options\" >> cmd.txt
+    echo "$0" "$@" >> cmd.txt #TODO: preserve quotes that may be in args
+    fi
   arg_parse "$@"
   write_condor_submission_files $raw_data_dir
   mkdir logs
