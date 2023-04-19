31c5f2864bfc6f3f3a31f2c208087f2be2cfac0e
Ben <s.benjamin@technion.ac.il>
2023-04-19 15:11:30 +0300

    add log file

--- run_on_zeus/bismark_wgbs/run_bismark_genome_preparation.sh
+++ run_on_zeus/bismark_wgbs/run_bismark_genome_preparation.sh
@@ -7,8 +7,9 @@
 #PBS  -l select=1:ncpus=20
 #PBS  -l select=mem=64gb
 #PBS  -l walltime=24:00:00
+
 PBS_O_WORKDIR=/home/s.benjamin/extra_storage_1TB/genomic_reference_data/hg38
 cd $PBS_O_WORKDIR
 
 conda activate /home/s.benjamin/micromamba/envs/wgbs_bismark_pipeline_2023
-bismark_genome_preparation --bowtie2 --parallel 10 --verbose /home/s.benjamin/extra_storage_1TB/genomic_reference_data/hg38
+bismark_genome_preparation --bowtie2 --parallel 10 --verbose /home/s.benjamin/extra_storage_1TB/genomic_reference_data/hg38 > bismark_genome_preparation.log 2>&1
