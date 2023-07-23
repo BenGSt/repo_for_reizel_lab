2e7ecfcb8b38ca8d9ef0f430666d8acba3f01c69
Ben <s.benjamin@technion.ac.il>
2023-07-23 16:30:23 +0300

    start work on atlas RRBS v2 -prepare mm9 BS genome

---
+++ run_on_zeus/ovatin_rrbs/run_bismark_genome_preparation_mm9.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PBS  -N  bismark_genome_preperation
+#PBS  -q  zeus_all_q
+#PBS  -m  abe
+#PBS  -M  s.benjamin@technion.ac.il
+#PBS  -l select=1:ncpus=20
+#PBS  -l select=mem=64gb
+#PBS  -l walltime=24:00:00
+
+PBS_O_WORKDIR=/home/s.benjamin/genomic_reference_data/Mus_musculus/UCSC/mm9/Sequence/WholeGenomeFasta/
+cd $PBS_O_WORKDIR
+
+/home/s.benjamin/other_software/mambaforge/bin/activate  /home/s.benjamin/micromamba/envs/wgbs_bismark_pipeline_2023
+echo running"bismark_genome_preparation --bowtie2 --parallel 10 --verbose /home/s.benjamin/genomic_reference_data/Mus_musculus/UCSC/mm9/Sequence/WholeGenomeFasta/ >> bismark_genome_preparation.log 2>&1" > bismark_genome_preparation.log
+bismark_genome_preparation --bowtie2 --parallel 10 --verbose /home/s.benjamin/genomic_reference_data/Mus_musculus/UCSC/mm9/Sequence/WholeGenomeFasta/ >> bismark_genome_preparation.log 2>&1
