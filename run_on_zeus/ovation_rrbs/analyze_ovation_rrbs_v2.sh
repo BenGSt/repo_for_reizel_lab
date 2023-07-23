d325cccfb0d7556dd5113850c8b88cc9f9ee0a32
Ben <s.benjamin@technion.ac.il>
2023-07-24 02:03:54 +0300

    start work on zeus RRBS v2 -prepare mm9 BS genome [V] - write a script to generate PBS submission files [V] -test analyze_ovation_rrbs_v2.sh [V]

--- run_on_zeus/ovation_rrbs/analyze_ovation_rrbs_v2.sh
+++ run_on_zeus/ovation_rrbs/analyze_ovation_rrbs_v2.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
 GENOMIC_REFERENCE_DATA=/home/s.benjamin/genomic_reference_data
-
+DIVERSITY_TRIM_SCRIPT=/home/s.benjamin/bioinformatics_software/NuMetRRBS/trimRRBSdiversityAdaptCustomers.py
+N_CORES=20
+TILE_SIZE=100
+TILE_MIN_COV=10
 main() {
   arg_parse "$@"
-
-  SCRIPT_NAME=$(echo $0 | awk -F / '{print $NF}')
-  echo \###################$SCRIPT_NAME \($(date)\)#############
-  echo running: $SCRIPT_NAME "$@"
-  date
-  echo hostname: $(hostname)
-  echo \########################################################
-  echo
-
-
-  select_genome
 
   #activate conda environment
   eval "$(micromamba shell hook --shell=bash)"
   micromamba activate /home/s.benjamin/micromamba/envs/ovation_rrbs_pipeline_2022
 
+  script_name=$(echo $0 | awk -F / '{print $NF}')
+  echo \###################$script_name \($(date)\)#############
+  echo running: $script_name "$@"
+  date
+  echo hostname: $(hostname)
+  echo \########################################################
+  echo
+
+  select_genome
   time trim_illumina_adapters
   time trim_diversity_adaptors
   time align_to_genome
   ##TODO: remove PCR duplicates (optional)
   time methylation_calling
-  time combine_methylation_coverage_to_tiles 100 10 #<tile_size> <min_coverage>
+  time combine_methylation_coverage_to_tiles $TILE_SIZE $TILE_MIN_COV
 
-  echo \##############finished $SCRIPT_NAME \($(date)\)#############
+  echo \########################################################
+  echo finished $script_name \($(date)\)
 }
 
 select_genome() {
   if [[ $genome == "mm10" ]]; then
     bismark_genome_location=$GENOMIC_REFERENCE_DATA/from_huji/mm10/Sequence/WholeGenomeFasta/
+    genome_tiles=/home/s.benjamin/genomic_reference_data/from_huji/mm10/mm10_${TILE_SIZE}bp_tiles.bed
   elif [[ $genome == "mm9" ]]; then
     bismark_genome_location=$GENOMIC_REFERENCE_DATA/Mus_musculus/UCSC/mm9/Sequence/WholeGenomeFasta/
+    genome_tiles=/home/s.benjamin/genomic_reference_data/Mus_musculus/UCSC/mm9/mm9_${TILE_SIZE}bp_tiles.bed
   elif [[ $genome == "hg38" ]]; then
     bismark_genome_location=$GENOMIC_REFERENCE_DATA/hg38/
+    genome_tiles=/home/s.benjamin/genomic_reference_data/hg38/hg38_${TILE_SIZE}bp_tiles.bed
   else
     echo ERROR: genome $genome not supported
     exit 1
@@ -52,10 +57,10 @@ select_genome() {
 #  --cores 4 would then be: 4 (read) + 4 (write) + 4 (Cutadapt) + 2 (extra Cutadapt) + 1 (Trim Galore) = 15, and so forth.
 #'
 trim_illumina_adapters() {
-  if [[ $READ_TYPE == "single_end" ]]; then
-    cmd=$(echo ${TRIM_GALORE} --adapter AGATCGGAAGAGC $INPUT_FASTQ --cores $N_CORES --fastqc)
+  if [[ $read_type == "single_end" ]]; then
+    cmd=$(echo trim_galore --adapter AGATCGGAAGAGC $input_fastq --cores $N_CORES --fastqc)
   else
-    cmd=$(echo ${TRIM_GALORE} --paired --adapter AGATCGGAAGAGC --adapter2 AAATCAAAAAAAC $INPUT_FASTQ_1 $INPUT_FASTQ_2 \
+    cmd=$(echo trim_galore --paired --adapter AGATCGGAAGAGC --adapter2 AAATCAAAAAAAC $input_fastq_1 $input_fastq_2 \
       --cores $N_CORES --fastqc)
   fi
   echo runnig: $cmd
@@ -71,9 +76,9 @@ trim_illumina_adapters() {
 #	#note from trim_galore manual
 #		#It seems that --cores 4 could be a sweet spot, anything above has diminishing returns.
 #		#--cores 4 would then be: 4 (read) + 4 (write) + 4 (Cutadapt) + 2 (extra Cutadapt) + 1 (Trim Galore) = 15, and so forth.
-#	echo \###################$SCRIPT_NAME \($(date)\)#############
+#	echo \###################$script_name \($(date)\)#############
 #	echo runnig: trim_galore --paired --adapter AGATCGGAAGAGC --adapter2 AAATCAAAAAAAC $1 $2 --cores $N_CORES --fastqc \($(date)\)
-#	${TRIM_GALORE} --paired --adapter AGATCGGAAGAGC --adapter2 AAATCAAAAAAAC $1 $2 --cores $N_CORES --fastqc
+#	trim_galore --paired --adapter AGATCGGAAGAGC --adapter2 AAATCAAAAAAAC $1 $2 --cores $N_CORES --fastqc
 #	echo \########################################################
 #}
 #
@@ -85,45 +90,40 @@ trim_illumina_adapters() {
 #	#note from trim_galore manual
 #		#It seems that --cores 4 could be a sweet spot, anything above has diminishing returns.
 #		#--cores 4 would then be: 4 (read) + 4 (write) + 4 (Cutadapt) + 2 (extra Cutadapt) + 1 (Trim Galore) = 15, and so forth.
-#	echo \###################$SCRIPT_NAME \($(date)\)#############
+#	echo \###################$script_name \($(date)\)#############
 #	echo runnig: trim_galore --adapter AGATCGGAAGAGC $1 --cores $N_CORES \($(date)\)
-#	${TRIM_GALORE} --adapter AGATCGGAAGAGC $1  --cores $N_CORES --fastqc
+#	trim_galore --adapter AGATCGGAAGAGC $1  --cores $N_CORES --fastqc
 #	echo \########################################################
 #}
 #
 
 trim_diversity_adaptors() {
   #https://github.com/nugentechnologies/NuMetRRBS#diversity-trimming-and-filtering-with-nugens-diversity-trimming-scripts
-  echo \###################$SCRIPT_NAME \($(date)\)#############
-  if [[ $READ_TYPE == "single_end" ]]; then
-    TRIM_GALORE_OUTPUT=$(echo $INPUT_FASTQ | awk -F / '{print $NF}' | sed 's/\(\.fastq\|.fq\)\.gz/_trimmed.fq.gz/')
-    echo runnig: python2 $DIVERSITY_TRIM_SCRIPT -1 $TRIM_GALORE_OUTPUT
-    python2 $DIVERSITY_TRIM_SCRIPT -1 $TRIM_GALORE_OUTPUT
+  if [[ $read_type == "single_end" ]]; then
+    trim_galore_output=$(echo $input_fastq | awk -F / '{print $NF}' | sed 's/\(\.fastq\|.fq\)\.gz/_trimmed.fq.gz/')
+    echo runnig: python2 $DIVERSITY_TRIM_SCRIPT -1 $trim_galore_output
+    python2 $DIVERSITY_TRIM_SCRIPT -1 $trim_galore_output
   else
-    TRIM_GALORE_OUTPUT_1=$(echo $INPUT_FASTQ_1 | awk -F / '{print $NF}' | sed 's/\(\.fastq\|.fq\)\.gz/_val_1.fq.gz/')
-    TRIM_GALORE_OUTPUT_2=$(echo $INPUT_FASTQ_2 | awk -F / '{print $NF}' | sed 's/\(\.fastq\|.fq\)\.gz/_val_2.fq.gz/')
-    echo runnig: python2 $DIVERSITY_TRIM_SCRIPT -1 $TRIM_GALORE_OUTPUT_1 -2 $TRIM_GALORE_OUTPUT_2
-    python2 $DIVERSITY_TRIM_SCRIPT -1 $TRIM_GALORE_OUTPUT_1 -2 $TRIM_GALORE_OUTPUT_2
+    trim_galore_output_1=$(echo $input_fastq_1 | awk -F / '{print $NF}' | sed 's/\(\.fastq\|.fq\)\.gz/_val_1.fq.gz/')
+    trim_galore_output_2=$(echo $input_fastq_2 | awk -F / '{print $NF}' | sed 's/\(\.fastq\|.fq\)\.gz/_val_2.fq.gz/')
+    echo runnig: python2 $DIVERSITY_TRIM_SCRIPT -1 $trim_galore_output_1 -2 $trim_galore_output_2
+    python2 $DIVERSITY_TRIM_SCRIPT -1 $trim_galore_output_1 -2 $trim_galore_output_2
   fi
-
-  echo \########################################################
 }
 
 align_to_genome() {
-  BISMARK_GENOME_LOCATION=/utemp/s.benjamin/genomic_reference_data/from_huji/mm10/Sequence/WholeGenomeFasta
-  if [[ $READ_TYPE == "single_end" ]]; then
-    TRIM_DIVERSITY_OUTPUT=$(echo $TRIM_GALORE_OUTPUT | sed 's/\.gz/_trimmed.fq.gz/')
-    COMMAND=$(echo $BISMARK $non_directional --multicore $N_CORES --bowtie2 $BISMARK_GENOME_LOCATION $TRIM_DIVERSITY_OUTPUT)
+  if [[ $read_type == "single_end" ]]; then
+    trim_diversity_output=$(echo $trim_galore_output | sed 's/\.gz/_trimmed.fq.gz/')
+    cmd=$(echo bismark $non_directional --parallel $(($N_CORES / 4))--bowtie2 $bismark_genome_location $trim_diversity_output)
   else
-    TRIM_DIVERSITY_OUTPUT_1=$(echo $TRIM_GALORE_OUTPUT_1 | sed 's/\.gz/_trimmed.fq.gz/')
-    TRIM_DIVERSITY_OUTPUT_2=$(echo $TRIM_GALORE_OUTPUT_2 | sed 's/\.gz/_trimmed.fq.gz/')
-    COMMAND=$(echo $non_directional --multicore $N_CORES --bowtie2 $BISMARK_GENOME_LOCATION -1 $TRIM_DIVERSITY_OUTPUT_1 -2 $TRIM_DIVERSITY_OUTPUT_2)
+    trim_diversity_output_1=$(echo $trim_galore_output_1 | sed 's/\.gz/_trimmed.fq.gz/')
+    trim_diversity_output_2=$(echo $trim_galore_output_2 | sed 's/\.gz/_trimmed.fq.gz/')
+    cmd=$(echo $non_directional --parallel $(($N_CORES / 4))--bowtie2 $bismark_genome_location -1 $trim_diversity_output_1 -2 $trim_diversity_output_2)
   fi
 
-  echo \###################$SCRIPT_NAME \($(date)\)#############
-  echo $SCRIPT_NAME runnig: $COMMAND \($(date)\)
-  $COMMAND
-  echo \########################################################
+  echo runnig: $cmd \($(date)\)
+  $cmd
+
 }
 
 # remove PCR duplicates()
@@ -132,19 +132,18 @@ align_to_genome() {
 # }
 
 methylation_calling() {
-  if [[ $READ_TYPE == "single_end" ]]; then
-    ALIGNMENT_OUTPUT=$(echo $TRIM_DIVERSITY_OUTPUT | sed 's/\.fq\.gz/_bismark_bt2.bam/')
+  if [[ $read_type == "single_end" ]]; then
+    alignment_output=$(echo $trim_diversity_output | sed 's/\.fq\.gz/_bismark_bt2.bam/')
     #By default, this mode will only consider cytosines in CpG context, but it can be extended to cytosines in any sequence context by using the option --CX
-    COMMAND=$(echo bismark_methylation_extractor --multicore $N_CORES --bedGraph --buffer_size 10G --output methylation_extractor_output $ALIGNMENT_OUTPUT)
+    cmd=$(echo bismark_methylation_extractor --multicore $(($N_CORES / 3)) --bedGraph --buffer_size 10G --output methylation_extractor_output $alignment_output)
   else
-    ALIGNMENT_OUTPUT=$(echo $TRIM_DIVERSITY_OUTPUT_1 | sed 's/\.fq\.gz/_bismark_bt2_pe.bam/')
-    COMMAND=$(echo bismark_methylation_extractor -p --multicore $N_CORES --bedGraph --buffer_size 10G --output methylation_extractor_output $ALIGNMENT_OUTPUT)
+    alignment_output=$(echo $trim_diversity_output_1 | sed 's/\.fq\.gz/_bismark_bt2_pe.bam/')
+    cmd=$(echo bismark_methylation_extractor -p --multicore $(($N_CORES / 3)) --bedGraph --buffer_size 10G --output methylation_extractor_output $alignment_output)
   fi
 
-  echo \###################$SCRIPT_NAME \($(date)\)#############
-  echo $SCRIPT_NAME runnig: $COMMAND
-  $COMMAND
-  echo \########################################################
+  echo runnig: $cmd
+  $cmd
+
 }
 
 combine_methylation_coverage_to_tiles() {
@@ -152,18 +151,21 @@ combine_methylation_coverage_to_tiles() {
   # adapted from Adam's script:
   # to make WholeGenome_100bpTiles.bed :
   # cat mm9.chrom.sizes | grep -v -P 'X|Y|M' | awk '{OFS="\t"; for (i=1; i<=$2; i+=100) print $1,i,i+100-1}' > WholeGenome_100bpTiles.bed
-  TILE_SIZE=$1
-  MIN_COVERAGE=$2
+  tile_size=$1
+  min_coverage=$2
+
   cd methylation_extractor_output/
-  METH_CALLING_OUTPUT=$(ls | grep cov.gz)
+  meth_call_output=$(ls | grep cov.gz)
+
   # 100bp tiles variant 2: First calculate the tiles and then remove tiles with total coverage < 10
-  FileOut=$(echo ${METH_CALLING_OUTPUT} | awk -v tile_size=$TILE_SIZE -F "." '{print $1 "_" tile_size "bp_tiles.bed" }')
-  bedtools intersect -a /utemp/s.benjamin/genomic_reference_data/mm10_whole_genome_${TILE_SIZE}bpTiles.bed -b ${METH_CALLING_OUTPUT} -wa -wb | awk -v cov=${MIN_COVERAGE} -v tileSize=${TILE_SIZE} 'BEGIN {OFS="\t"; Prev=-1} {if ($2 == Prev) {T=T+$8+$9; M=M+$8} else {if (Prev!=-1 && T>=cov) {print PrevChr,Prev,Prev+tileSize-1,M/T};T=$8+$9; M=$8;}; Prev=$2; PrevChr=$1}' >../${FileOut}
-  #nor sure what this is for
+  file_out=$(echo meth_call_output | awk -v tile_size=$tile_size -F "." '{print $1 "_" tile_size "bp_tiles.bed" }')
+  bedtools intersect -a $genome_tiles -b $meth_call_output -wa -wb | awk -v cov=$min_coverage -v tileSize=$tile_size 'BEGIN {OFS="\t"; Prev=-1} {if ($2 == Prev) {T=T+$8+$9; M=M+$8} else {if (Prev!=-1 && T>=cov) {print PrevChr,Prev,Prev+tileSize-1,M/T};T=$8+$9; M=$8;}; Prev=$2; PrevChr=$1}' >../${file_out}
+
+  #unite tiles from different samples into big table
   #bedtools unionbedg -names `du -a -L | grep Tiles | awk '{print $2}' | sort | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}'` -header -filler NA -i `du -a -L | grep Tiles | awk '{print $2}' | sort` > 100bpTiles_Tiles_Cov10_Tissues.bed
 
-  echo \###################$SCRIPT_NAME \($(date)\)#############
-  echo $SCRIPT_NAME runnig: combine_methylation_coverage_to_tiles $1 $2
+  echo \###################$script_name \($(date)\)#############
+  echo $script_name runnig: combine_methylation_coverage_to_tiles $1 $2
   echo \########################################################
 
 }
@@ -180,29 +182,29 @@ arg_parse() {
   while [[ $# -gt 0 ]]; do
     case $1 in
     -single-end)
-      READ_TYPE="single_end"
-      shift # past argument
+      read_type="single_end"
+      shift
       ;;
     -paired-end)
-      READ_TYPE="paired_end"
-      shift # past argument
+      read_type="paired_end"
+      shift
       ;;
     -input_fastq_file)
-      INPUT_FASTQ="$2"
+      input_fastq="$2"
       shift # past argument
       shift # past value
       ;;
     -paired_input_fastq_files)
-      INPUT_FASTQ_1="$2"
-      shift # past argument
-      INPUT_FASTQ_2="$2"
-      shift # past argument2
-      shift # past value
+      input_fastq_1="$2"
+      shift
+      input_fastq_2="$2"
+      shift 2
+      shift
       ;;
     -n_cores)
       N_CORES="$2"
-      shift # past argument
-      shift # past value
+      shift
+      shift
       ;;
     -non-directional)
       non_directional="--non_directional"
