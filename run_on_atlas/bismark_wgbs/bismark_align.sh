bda4e1cc81cc10dfd966641fe99f42a1eb1ad3e7
Ben <s.benjamin@technion.ac.il>
2023-09-03 19:44:01 +0300

    remove output from previous run to not corrupt restarted jobs.

--- run_on_atlas/bismark_wgbs/bismark_align.sh
+++ run_on_atlas/bismark_wgbs/bismark_align.sh
@@ -28,6 +28,7 @@ main() {
 
   echo "cleaning up any previous runs so that restarted jobs aren't corrupted"
   rm -v $(find . -name "*.bam" -o -name "*unmapped_reads*" -o -name "*ambiguous_reads*" -o -name "*C_to_T*" -o -name "*G_to_A*")
+  echo
 
   #set bismark_genome_location
   if [[ $genome == "mm10" ]]; then
