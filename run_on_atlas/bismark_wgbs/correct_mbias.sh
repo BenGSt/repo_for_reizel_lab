1c0862e3439daa1b4f6465803a371235920876b0
Ben <s.benjamin@technion.ac.il>
2023-09-07 14:39:46 +0300

    document

--- run_on_atlas/bismark_wgbs/correct_mbias.sh
+++ run_on_atlas/bismark_wgbs/correct_mbias.sh
@@ -76,7 +76,8 @@ EOF
 
 help() {
   cat <<EOF
-  $script_name --biased_dir <path> {at least one of: --ignore_r1 <int> --ignore_r2  --ignore_3prime <int> --ignore_3prime_r2 <int>} [--output-dir <output_dir>]
+USAGE: $script_name --biased_dir <path> {at least one of: --ignore_r1 <int> --ignore_r2  --ignore_3prime <int> --ignore_3prime_r2 <int>} [--output-dir <output_dir>]
+
 obligatory options:
    --biased_dir <path>
 at least one of the following:
